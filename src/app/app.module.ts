a625403d0e3ef3368a2d13b677465b2a39cbc511
Jairo Sousa <jaironsousa@gmail.com>
2017-09-01 20:46:28 -0300

    Desenvolvido Header, mains ate sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SobreComponent } from "./main/sobre/sobre.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
